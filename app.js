33589b4dd0478669d3fd6deb32a182515af710a9
sdonich <sdonich@gmail.com>
2018-12-04 22:07:31 +0200

    add mylist

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const tasklist = require('./data/tasklist.json');
 const app = express();
 
+
+app.set('view engine', 'pug');
+
+
 app.get('/', (req, res) => {
-  res.send('hello');
+  res.render('mylist', {tasklist});
 });
 
 app.listen(3000, () => console.log('Server is running'));
