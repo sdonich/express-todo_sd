51e300eca09530d3119555db810cae7cb3cf1c63
sdonich <sdonich@gmail.com>
2018-12-07 01:47:26 +0200

    body-parse fight

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const tasklist = require('./data/tasklist.json');
 const bodyParser = require('body-parser');
-var jsonParser = bodyParser.json();
+const fs = require('fs');
+const path = require('path');
+const jsonParser = bodyParser.json();
 const app = express();
+
+// const urlencodedParser = bodyParser.urlencoded({extended: false});
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/public'));
 
@@ -10,7 +16,23 @@ app.set('view engine', 'pug');
 
 app.post('/edit', jsonParser, (req, res) => {
 
-  console.log(req.body);
+  console.log(req.body.key);
+  // let task = {};
+  // task.title = req.body.key;
+  // fs.readFile(path.resolve('data', 'tasklist.json'), (err, data) => {
+  //   // console.log(data);
+  //   let json = JSON.parse(data);
+  //   json.push(task);
+  //   console.log(json);
+  //   const xxx = JSON.stringify(json);
+  //   fs.writeFile(path.resolve('data', 'tasklist.json'), xxx, (err) => {
+  //     // console.log(tasklist);
+  //     if (err) console.log('f');
+  //   });
+
+  // });
+  res.render('mylist', { tasklist });
+  // res.resume();
   // req.end(data, 'utf-8', () => {
   //   console.log(data);
   // });
--- public/main.js
+++ public/main.js
@@ -6,10 +6,12 @@ let button = document.querySelectorAll('button');
 function sendData(data) {
   let xhr = new XMLHttpRequest();
   xhr.responseType = 'json';
+  
 
   xhr.open('POST', 'http://localhost:3000/edit');
-
-
+  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  // console.log(data);
+  
 
   xhr.send(data);
 
@@ -23,20 +25,31 @@ button.forEach((item) => {
     let task = evt.target.parentElement.querySelector('div');
 
     let edit = document.createElement('input');
+    let form = document.querySelector('form');
     edit.classList.add('edit');
     edit.placeholder = task.textContent;
-    // edit
-    document.body.append(edit);
+    edit.setAttribute('name', 'key');
+    edit.setAttribute('type', 'text');
+    form.append(edit);
+
+    document.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+      console.log(form);
+      let data = new FormData(form);
+        
+      sendData(data);
+    });
+
     document.addEventListener('keydown', (evt) => {
+      // evt.preventDefault();
       if (evt.keyCode === 13) {
-        let editTask = edit.value;
+        evt.preventDefault();
         // console.log(editTask);
         
         // console.log(typeof xx);
-        let xxx = new FormData();
-        xxx.append('aaaa', 'dddd');
-        console.log(xxx);
-        sendData(xxx);
+        let data = new FormData(form);
+        
+        sendData(data);
       }
     });
     // edit();
