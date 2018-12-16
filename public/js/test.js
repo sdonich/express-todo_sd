ffd0a7dd817fbac84c0cb1c23985e36f180a656b
sdonich <sdonich@gmail.com>
2018-12-16 21:15:04 +0300

    добавляю create task

---
+++ public/js/test.js
@@ -0,0 +1,12 @@
+// 'use strict';
+
+// let createField = document.querySelector('.create_field');
+
+// createField.addEventListener('click', (evt) => {
+//   // createField.textContent = '';
+//   // createField.style.color = 'black';
+//   // createField.style.minHeight = '40px';
+
+
+
+// });
